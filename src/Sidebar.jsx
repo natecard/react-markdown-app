4eedace03f8a7d12ecc9faf23ff83b0cb5ab8547
Nate Card <ncard24@gmail.com>
2023-01-21 07:26:08 -0500

    Styling complete on sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,14 +4,14 @@ export default function Sidebar(props) {
   const noteElements = props.notes.map((note, body, index) => (
     <div key={note.id}>
       <div
-        className={`title flex cursor-pointer items-center overflow-hidden p-4 ${
-          note.id === props.currentNote.id ? 'selected-note bg-gray-600' : ''
+        className={`title flex cursor-pointer items-center overflow-hidden rounded-l-md border-b-2 border-b-slate-400 p-4 ${
+          note.id === props.currentNote.id
+            ? 'selected-note bg-slate-400 text-neutral-900'
+            : ''
         }`}
         onClick={() => props.setCurrentNoteId(note.id)}
       >
-        <h4
-          className="p-2 mr-auto overflow-hidden text-sm font-normal text-gray-400 text-snippet text-ellipsis whitespace-nowrap "
-        >
+        <h4 className="p-2 mr-auto overflow-hidden text-sm font-normal text-snippet text-ellipsis whitespace-nowrap text-neutral-900 ">
           {note.body.split('\n')[0]}
         </h4>
         <button
@@ -29,7 +29,7 @@ export default function Sidebar(props) {
       <div className="flex items-center justify-around sidebar--header">
         <h3 className="p-3 text-3xl">Notes</h3>
         <button
-          className="w-8 h-8 text-white bg-gray-400 border-none rounded cursor-pointer new-note"
+          className="w-8 h-8 text-white border-none rounded cursor-pointer new-note bg-slate-500"
           onClick={props.newNote}
         >
           +
